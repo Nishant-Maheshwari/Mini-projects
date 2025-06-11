6aff2327eba693b78281d3134e3736e9553c27df
Nishant Maheshwari <nishant2622@gmail.com>
2025-06-12 01:44:05 +0530

    feat: unit toggle for BMI calculator (kg/m ↔ lb/in)

--- BMI-calc.js
+++ BMI-calc.js
@@ -1,13 +1,36 @@
 let Height = document.querySelector('.js-height');
 let Weight = document.querySelector('.js-weight');
-let weightUnit = true
-function toggleWeight(){
+let toggelbutton = document.querySelector('.js-toggle')
+let units = true
 
+function toggelButton(){
+  units = !units
+  if(!units){
+  toggelbutton.textContent = 'lb/inches'
+ 
+  } 
+
+  if(units){
+     toggelbutton.textContent = 'Kg/meters'
+  
+  }
 }
+
 
 function calculate(){
-  let heightValue = Height.value/100 
-  let weightValue = Weight.value
- let result = weightValue/(heightValue*heightValue)
+  let heightValue = parseFloat(Height.value) 
+  let weightValue = parseFloat(Weight.value)
+  let heightInMeters;
+  let weightInkilo;
+  if(!units){
+    heightInMeters = heightValue * 0.0254;
+    weightInkilo = weightValue * 0.453592;
+  }
+  if(units){
+    heightInMeters = heightValue;
+    weightInkilo = weightValue;
+  }
+
+ let result = weightInkilo/(heightInMeters * heightInMeters)
   document.querySelector('.js-result').innerHTML = result.toFixed(2)
-}
+} 
